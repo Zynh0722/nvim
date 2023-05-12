e426c714e8be21e6240b901481e67c637eb7a943
Zynh Ludwig <Zynh0722@gmail.com>
2023-05-12 04:00:33 -0700

    add git push

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>fo", function() vim.cmd('Git pull') end)
+vim.keymap.set("n", "<leader>po", function() vim.cmd('Git push') end)
