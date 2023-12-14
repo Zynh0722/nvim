b7b1127aac26d53dc161a388fcdeb2fcb9606f6e
Zynh0722 <Zynh0722@gmail.com>
2023-12-14 03:49:45 -0800

    switching to lazy extras manager

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,38 +1,49 @@
-return {}
--- return {
---   {
---     "neovim/nvim-lspconfig",
---     ---@class PluginLspOpts
---     opts = {
---       ---@type lspconfig.options
---       servers = {
---         -- pyright will be automatically installed with mason and loaded with lspconfig
---         rust_analyzer = {
---           mason = false,
---           settings = {
---             ["rust-analyzer"] = {
---               imports = {
---                 granularity = {
---                   group = "module",
---                 },
---               },
---               cargo = {
---                 buildScripts = {
---                   enable = false,
---                 },
---               },
---               files = {
---                 excludeDirs = {
---                   "pe-template",
---                 },
---               },
---               procMacro = {
---                 enable = true,
---               },
---             },
---           },
---         },
---       },
---     },
---   },
--- }
+-- return {}
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      -- -@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        rust_analyzer = {
+          mason = false,
+          settings = {
+            ["rust-analyzer"] = {
+              rustfmt = {
+                overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
+              },
+              imports = {
+                granularity = {
+                  group = "module",
+                },
+              },
+              cargo = {
+                buildScripts = {
+                  enable = false,
+                },
+              },
+              files = {
+                excludeDirs = {
+                  "pe-template",
+                },
+              },
+              procMacro = {
+                enable = true,
+              },
+            },
+          },
+        },
+        tailwindcss = {
+          init_options = {
+            userLanguages = {
+              rust = "html",
+            },
+          },
+          filetypes = { "rust" },
+        },
+      },
+    },
+  },
+}
