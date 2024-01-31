ebae08f75915952d0b0e55f44d76a89f92f86b0b
Zynh0722 <Zynh0722@gmail.com>
2024-01-31 14:40:43 -0800

    updates and stuff

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.g.editorconfig = true
+vim.cmd("filetype plugin on")
+vim.cmd("filetype indent off")
+
 if vim.loader then
   vim.loader.enable()
 end
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,14 +4,18 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        groups = {
-          background = "none",
+        -- groups = {
+        --   background = "none",
+        -- },
+        styles = {
+          transparency = true,
         },
       })
     end,
   },
   {
     "nyoom-engineering/oxocarbon.nvim",
+    event = "VeryLazy",
   },
 
   -- Configure LazyVim to load rose-pine
