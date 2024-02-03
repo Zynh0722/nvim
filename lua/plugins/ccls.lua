4cf2aae99d0011a71e7f8092b3e914b18d005386
Zynh0722 <Zynh0722@gmail.com>
2024-02-03 13:46:45 -0800

    ccls is off idkwhy

---
+++ lua/plugins/ccls.lua
@@ -0,0 +1,10 @@
+return {
+  --   {
+  --     "ranjithshegde/ccls.nvim",
+  --     config = function()
+  --       require("ccls").setup({ lsp = { use_defaults = true } })
+  --     end,
+  --     event = "VeryLazy",
+  --     dependencies = { "nvim-lspconfig" },
+  --   },
+}
