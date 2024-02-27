5757ca54fa9308358821a6622cca6a3cbc509ffc
Zynh0722 <Zynh0722@gmail.com>
2024-02-27 04:46:34 -0800

    mac updates

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,31 +9,31 @@ return {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         rust_analyzer = {
           mason = false,
-          settings = {
-            ["rust-analyzer"] = {
-              -- rustfmt = {
-              --   overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
-              -- },
-              imports = {
-                granularity = {
-                  group = "module",
-                },
-              },
-              cargo = {
-                buildScripts = {
-                  enable = false,
-                },
-              },
-              files = {
-                excludeDirs = {
-                  "pe-template",
-                },
-              },
-              procMacro = {
-                enable = true,
-              },
-            },
-          },
+          -- settings = {
+          --   ["rust-analyzer"] = {
+          --     -- rustfmt = {
+          --     --   overrideCommand = { "leptosfmt", "--stdin", "--rustfmt" },
+          --     -- },
+          --     imports = {
+          --       granularity = {
+          --         group = "module",
+          --       },
+          --     },
+          --     cargo = {
+          --       buildScripts = {
+          --         enable = false,
+          --       },
+          --     },
+          --     files = {
+          --       excludeDirs = {
+          --         "pe-template",
+          --       },
+          --     },
+          --     procMacro = {
+          --       enable = true,
+          --     },
+          --   },
+          -- },
         },
         tailwindcss = {
           init_options = {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,19 @@ return {
     "telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
+      "wesleimp/telescope-windowizer.nvim",
       build = "make",
       config = function()
-        require("telescope").load_extension("fzf")
+        local telescope = require("telescope")
+        telescope.setup({
+          extentions = {
+            windowizer = {
+              find_cmd = "rg",
+            },
+          },
+        })
+        telescope.load_extension("fzf")
+        telescope.load_extension("windowizer")
       end,
     },
   },
