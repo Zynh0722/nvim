d915829d018fbb949179abdf7c155b5fe6fc91f7
Zynh0722 <Zynh0722@gmail.com>
2024-03-15 05:22:22 -0700

    better mdx support

--- after/queries/markdown/injections.scm
+++ after/queries/markdown/injections.scm
@@ -1,7 +1,16 @@
 ;extends
 
+; Supporting dataviewjs
 (fenced_code_block
   ((info_string) @_lang
     (#match? @_lang "(dataviewjs)"))
   (code_fence_content) @javascript
 )
+
+; Supporting MDX
+((inline) @injection.content
+  (#lua-match? @injection.content "^%s*import")
+  (#set! injection.language "typescript"))
+((inline) @injection.content
+  (#lua-match? @injection.content "^%s*export")
+  (#set! injection.language "typescript"))
